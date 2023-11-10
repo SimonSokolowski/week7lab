1b371aca1b2dc24502bf6d8d722a13ea5a9bd0b4
Simon <simon.sokolowski@outlook.com>
2023-11-10 11:22:03 +0000

    final commit

--- main.cpp
+++ main.cpp
@@ -15,24 +15,59 @@ struct Employee{
 
     public:
 
-    void EmployeeSet(std::string firstName, std::string secondName, std::string email,
-    Employee* manager, std::string phoneNumber, int ID, int salary) {
+    Employee() : manager(nullptr), ID(0), salary(0) {}
+
+    Employee(std::string firstName, std::string secondName, std::string email, std::string phoneNumber, int ID, int salary) {
         this->firstName = firstName;
         this->secondName = secondName;
         this->email = email;
-        this->manager = manager;
         this->phoneNumber = phoneNumber;
         this->ID = ID;
         this->salary = salary;
     }
+    void setManager(Employee* manager) {
+        this->manager = manager;
+    }
     void employeeDetails() {
         std::cout << firstName << " ";
         std::cout << secondName << " ";
+        std::cout << ID << " ";
     }
     void employeeMnaager() {
         manager->employeeDetails();
     }
 };
+
+void createUsers() {
+    std::cout << "how many users? " << std::endl;
+    int users;
+    std::cin >> users;
+    Employee* employeeArr = new Employee[users];
+
+    for (int i=0; i<users; i++) {
+        std::string firstName, lastName, email, phoneNumber;
+        std::cout << "first name " << std::endl;
+        std::cin >> firstName;
+        std::cout << "last name " << std::endl;
+        std::cin >> lastName;
+        std::cout << "email " << std::endl;
+        std::cin >> email;
+        std::cout << "number " << std::endl;
+        std::cin >> phoneNumber;
+        
+        int id, salary;
+        std::cout << "id " << std::endl;
+        std::cin >> id;
+        std::cout << "salary " << std::endl;
+        std::cin >> salary;        
+
+        Employee employee(firstName, lastName, email, phoneNumber, id, salary);
+        employeeArr[i] = employee;
+    }
+    for (int i=0; i<users; i++) {
+        employeeArr[i].employeeDetails();
+    }
+}
 
 int main() {
     const double var = 2.4;
@@ -46,13 +81,15 @@ int main() {
     //reverse(name, 5);
     //std::cout << name;
 
-    Employee employee1;
-    Employee employee2;
-    Employee employee3;
-    employee1.EmployeeSet("Greg", "Gr", "greg@mail.com", &employee2, "2342342", 12, 12);
-    employee2.EmployeeSet("Grog", "Gg", "greg@mail.com", &employee2, "2342342", 12, 12);
-    employee1.employeeDetails();
-    employee1.employeeMnaager();
+    //Employee employee1("Greg", "Gr", "greg@mail.com", "2342342", 12, 12);
+    //Employee employee2("Grog", "Gg", "greg@mail.com", "2342342", 122, 122);
+    //Employee employee3("henry", "hr", "henry@mail.com", "2342342", 123, 123);
+//
+    //employee1.setManager(&employee2);
+    //employee1.employeeDetails();
+    //employee1.employeeMnaager();
+
+    createUsers();
     return 0;
 }
 
